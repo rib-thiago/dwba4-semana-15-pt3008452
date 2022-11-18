2566abd668671ce46a4e561cf78b9c3c8184e581
Thiago Ribeiro <72164141+rib-thiago@users.noreply.github.com>
2022-11-18 00:38:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import logging
+import traceback
+from flask import redirect
+
+import flask
+from replit import db
+
+app = flask.Flask(__name__)
+
+@app.errorhandler(500)
+def internal_server_error(e: str):
+    return flask.jsonify(error=str(e)), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def cadastroContatos():
+  try:
+    contatos = db.get('contatos', {}); 
+    print(contatos);
+    if flask.request.method == "POST":      
+      contatos[flask.request.form['email']] = {'nome': flask.request.form['nome'], 'telefone': flask.request.form['telefone'],'assunto': flask.request.form['assunto'], 'mensagem': flask.request.form['mensagem'],'resposta': flask.request.form.getlist('resposta')}
+    db['contatos'] = contatos
+    return flask.render_template('contatos.html', contatos=contatos)
+  except Exception as e:
+    logging.exception('failed to database')
+    flask.abort(500, description=str(e) + ': ' + traceback.format_exc())
+
+
+@app.route('/limparBanco', methods=['POST'])
+def limparBanco():
+  try:
+    del db["contatos"];
+    return flask.render_template('contatos.html')
+  except Exception as e:
+    logging.exception(e)
+    return flask.render_template('contatos.html')
+
+
+@app.route('/deletarContato/<email>', methods=['POST'])
+def deletarContato(email):
+    try:
+      contatos = db.get('contatos', {});
+      del contatos[email];
+      db['contatos'] = contatos;
+      return redirect('/');
+    except Exception as e:
+      logging.exception(e);
+      return flask.render_template('contatos.html');
+
+@app.route('/update/<email>', methods=['POST'])
+def update(email):
+  try:
+    if flask.request.method == "POST":
+      contatos = db.get('contatos', {});
+      contato = {};
+      contato = {'email': email,
+                  'nome': contatos[email]['nome'],
+                  'telefone': contatos[email]['telefone'],
+                  'assunto': contatos[email]['assunto'],
+                  'mensagem': contatos[email]['mensagem'],
+                  'resposta': contatos[email]['resposta']};
+      print(contato);
+      return flask.render_template('contato.html', contato=contato)
+  except Exception as e:
+      logging.exception(e);
+      return flask.render_template('contatos.html');
+    
+    
+app.run('0.0.0.0')
